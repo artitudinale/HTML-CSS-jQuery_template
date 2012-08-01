223ff35a69512400c6bf5009db3343dbcbedd0d5
alessandra <agarulli@yaoo.com>
2012-08-01 15:21:04 +0100

    changed on wed 15:20pm

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+
+//script for the navigation bar
+
+var timeout    = 500;
+var closetimer = 0;
+var ddmenuitem = 0;
+
+function menu_open()
+{  menu_canceltimer();
+   menu_close();
+   ddmenuitem = $(this).find('ul').css('visibility', 'visible');}
+
+function menu_close()
+{  if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');}
+
+function menu_timer()
+{  closetimer = window.setTimeout(menu_close, timeout);}
+
+function menu_canceltimer()
+{  if(closetimer)
+   {  window.clearTimeout(closetimer);
+      closetimer = null;}}
+
+$(document).ready(function()
+{  $('#menu > li').bind('mouseover', menu_open)
+   $('#menu > li').bind('mouseout',  menu_timer)});
+
+document.onclick = menu_close;
+
+
+
+
+
+
+//script for slideshow
+
+
+
 function slideSwitch() {
     var $active = $('#slideshow IMG.active');
 
